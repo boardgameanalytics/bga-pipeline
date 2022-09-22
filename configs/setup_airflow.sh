594f64e12d34758b8f200f66f2d603195820956c
Randy Nance <randynobx@gmail.com>
2022-09-22 08:44:40 -0400

    CHG: all config done before make up
    Database connection is now placed in configs/airflow_connections.json for
    automatic import to airflow on spin up.
    
    Updated README.md instructions.
    
    Added airflow_connections.json to gitignore to protect sensitive info.

--- configs/setup_airflow.sh
+++ configs/setup_airflow.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# Clear existing api connection
+# Clear existing and import API connection
 airflow connections delete api_bggxmlapi2
+airflow connections add 'api_bggxmlapi2' \
+    --conn-type 'http' \
+    --conn-host 'https://boardgamegeek.com/xmlapi2'
 
 # Import connection and variable files
-airflow connections import /opt/airflow/configs/airflow_connections.json
+if [[ -f /opt/airflow/configs/airflow_connections.json ]]; then
+    airflow connections import /opt/airflow/configs/airflow_connections.json
+fi;
 airflow variables import /opt/airflow/configs/airflow_variables.json
