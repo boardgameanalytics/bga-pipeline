cf709a68b8a6784ff86ffabf24b857bf43198d5f
Randy Nance <randynobx@gmail.com>
2022-10-02 18:03:42 -0400

    Unit tests for extract_xml.py

--- dags/py/extract_xml.py
+++ dags/py/extract_xml.py
@@ -5,18 +5,19 @@ Takes bgg game ids and generates batched xml files
 
 from pathlib import Path
 from math import ceil
-from requests import Response
+from typing import Generator
 from .bggxmlapi2 import fetch_game
 
 
-def save_file(path: Path, content: str) -> None:
+def save_file(path: Path, content: str) -> bool:
     """Save page to file"""
     path.parent.mkdir(parents=True, exist_ok=True)
     with open(path, 'w', encoding='utf-8') as file:
         file.write(content)
+    return path.exists()
 
 
-def scrape_game_pages(game_ids_list: list, batch_size: int) -> Response:
+def scrape_game_pages(game_ids_list: list, batch_size: int) -> Generator:
     """Fetch, save, and extract data from game pages
 
     Args:
@@ -42,12 +43,9 @@ def main(game_ids_file: Path, destination_dir: Path, batch_size: int) -> None:
         destination_dir (Path): Filepath of directory to save xml files
         batch_size (int): Number of games to include per API query
     """
-
-    # Load game ids
     with game_ids_file.open() as file:
         game_ids = file.read().split('\n')
 
-    # Scrape game data in batches
     for num, xml in enumerate(scrape_game_pages(game_ids, batch_size)):
         destination_path = destination_dir / f'bgg_games_batch_{str(num).zfill(2)}.xml'
         save_file(destination_path, xml)
--- tests/test_extract_xml.py
+++ tests/test_extract_xml.py
@@ -1,39 +1,57 @@
 from pathlib import Path
+from bs4 import BeautifulSoup
+from requests import Response
 from dags.py.extract_xml import save_file
+from dags.py.extract_xml import scrape_game_pages
+from dags.py.extract_xml import main
 
 
 def test_save_file() -> None:
     text = "This is a test file."
-    testfile = Path('testfile.txt')
+    tmp_file = Path('tmp_file.txt')
+    tmp_file.unlink(missing_ok=True)
 
-    testfile.unlink(missing_ok=True)
+    assert save_file(tmp_file, text)
+    assert tmp_file.exists(), 'File not created'
 
-    save_file(testfile, text)
-
-    assert testfile.exists(), 'File not created'
-
-    testfile.unlink()
+    tmp_file.unlink()
 
 
 def test_save_file_missing_dir() -> None:
     text = "This is a test file."
-    testfile = Path('test_assets/testfile.txt')
+    tmp_file = Path('test_assets/tmp_file.txt')
 
-    testfile.unlink(missing_ok=True)
-    if testfile.parent.exists():
-        testfile.parent.rmdir()
+    tmp_file.unlink(missing_ok=True)
+    if tmp_file.parent.exists():
+        tmp_file.parent.rmdir()
 
-    save_file(testfile, text)
+    assert save_file(tmp_file, text)
+    assert tmp_file.exists(), 'File not created'
 
-    assert testfile.exists(), 'File not created'
-
-    testfile.unlink()
-    testfile.parent.rmdir()
+    tmp_file.unlink()
+    tmp_file.parent.rmdir()
 
 
 def test_scrape_game_pages() -> None:
-    assert False
+    game_ids = ['187645', '220308']
+    test_res = list(scrape_game_pages(game_ids, 2))[0]
+
+    assert(isinstance(test_res, str)), 'Function did not return str object'
+    soup = BeautifulSoup(test_res, features='xml')
+    assert len([x for x in soup.items if x != '\n']) == 2
 
 
 def test_main():
-    assert False
+    tmp_dir = Path('tests/assets/tmp_dir')
+    game_ids_path = Path('tests/assets/test_game_ids.csv')
+
+    main(game_ids_file=game_ids_path,
+         destination_dir=tmp_dir,
+         batch_size=50)
+
+    saved_files = list(tmp_dir.glob(r'*.xml'))
+    print(saved_files)
+    assert len(saved_files) == 3
+    for file in saved_files:
+        file.unlink()
+    tmp_dir.rmdir()
