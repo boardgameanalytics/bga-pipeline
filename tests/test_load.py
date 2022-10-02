f38fc2e876b714ff3ec2d1a88f1a649914e0421c
Randy Nance <randynobx@gmail.com>
2022-10-02 09:56:24 -0400

    Added unit test for load.py

---
+++ tests/test_load.py
@@ -0,0 +1,22 @@
+"""Unit tests for load.py"""
+
+from pathlib import Path
+import sqlite3
+import pandas as pd
+from dags.py.load import load_table
+
+
+def test_load_table():
+    csv_path = Path('tests/assets/test_table.csv')
+    db_path = Path('tests/assets/test.db')
+
+    csv_df = pd.read_csv(csv_path)
+    db_path.unlink(missing_ok=True)
+
+    with sqlite3.connect(db_path) as db_conn:
+        load_table(csv_path=csv_path, engine=db_conn)
+
+        db_df = pd.read_sql_query("SELECT * FROM test_table", db_conn)
+        assert len(csv_df.compare(db_df)) == 0
+
+    db_path.unlink()
